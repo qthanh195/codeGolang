6ab19b7c828f4bf5dc33ea6f49647400d90b897a
quangthanhsaotruc <quangthanhsaotruc@gmail.com>
2024-06-10 13:06:21 +0700

    Modify code and add input error

--- DsSinhvien/DsSinhvien.go
+++ DsSinhvien/DsSinhvien.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type Student struct {
@@ -12,12 +17,19 @@ type Student struct {
 	ChemistryScore float32
 }
 
-// điểm trung bình của sinh viên
-func (s *Student) CalcAvgScore() float32 {
+func NewStudent(name string, mssv int, mathScore float32, physicsScore float32, chemistryScore float32) *Student {
+	return &Student{
+		Name:           name,
+		Mssv:           mssv,
+		MathScore:      mathScore,
+		PhysicsScore:   physicsScore,
+		ChemistryScore: chemistryScore,
+	}
+}
+func CalcAvgScore(s *Student) float32 {
 	return (s.MathScore*2 + s.PhysicsScore + s.ChemistryScore) / 4
 }
-
-func (s *Student) printInformation() {
+func printInformation(s *Student) {
 	fmt.Printf("| %v | %v | %v | %v | %v |\n", s.Name, s.Mssv, s.MathScore, s.PhysicsScore, s.ChemistryScore)
 }
 
@@ -25,22 +37,32 @@ type Classroom struct {
 	students []*Student
 }
 
-func (p *Classroom) AddStudent(student *Student) {
-	p.students = append(p.students, student)
-}
-
-func (p *Classroom) EditStudent(name string, mssv int, mathScore float32, physicsScore float32, chemistryScore float32) {
+func (p *Classroom) FindStudent(mssv int) *Student {
 	for _, st := range p.students {
 		if st.Mssv == mssv {
-			st.Name = name
-			st.MathScore = mathScore
-			st.PhysicsScore = physicsScore
-			st.ChemistryScore = chemistryScore
-			fmt.Println("Student information updated.")
-			return
+			// printInformation(st)
+			return st
 		}
 	}
-	fmt.Println("Student not found.")
+	return nil
+}
+
+func (p *Classroom) AddStudent(student *Student) {
+	st := p.FindStudent(student.Mssv)
+	if st == nil{
+		p.students = append(p.students, student)
+		fmt.Println("Students added.")
+	} else{
+		fmt.Println("Mssv already exists")
+	}	
+}
+
+func (p *Classroom) ModifyStudent(student *Student, studentUpdate *Student) {
+	student.Name = studentUpdate.Name
+	student.MathScore = studentUpdate.MathScore
+	student.PhysicsScore = studentUpdate.PhysicsScore
+	student.ChemistryScore = studentUpdate.ChemistryScore
+	fmt.Println("Student information updated.")
 }
 
 func (p *Classroom) DeleteStudent(mssv int) {
@@ -54,43 +76,62 @@ func (p *Classroom) DeleteStudent(mssv int) {
 	fmt.Println("Student not found.")
 }
 
-func (p *Classroom) FindStudent(mssv int) *Student {
-	for _, st := range p.students {
-		if st.Mssv == mssv {
-			st.printInformation()
-			return st
-		}
-	}
-	fmt.Println("Student not found.")
-	return nil
-}
-
 func (p *Classroom) FindStudentMaxAvgScore() {
 	if len(p.students) == 0 {
 		fmt.Println("No students in the classroom.")
 		return
 	}
 
-	max := p.students[0].CalcAvgScore()
+	max := CalcAvgScore(p.students[0])
 	index := 0
 	for i, st := range p.students {
-		if avg := st.CalcAvgScore(); avg > max {
+		if avg := CalcAvgScore(st); avg > max {
 			max = avg
 			index = i
 		}
 	}
 	fmt.Printf("Average score: %v\n", max)
-	p.students[index].printInformation()
+	printInformation(p.students[index])
 }
 
-func NewStudent(name string, mssv int, mathScore float32, physicsScore float32, chemistryScore float32) *Student {
-	return &Student{
-		Name:           name,
-		Mssv:           mssv,
-		MathScore:      mathScore,
-		PhysicsScore:   physicsScore,
-		ChemistryScore: chemistryScore,
+func inputString(prompt string) string {
+	reader := bufio.NewReader(os.Stdin) // tạo đối tượng reader để đọc dữ liệu từ bàn phím
+	regex := regexp.MustCompile(`^[a-zA-Z ]+$`) //tạo regex cho phép các ký tự chữ cái (a-z, A-Z) và " "
+	for {
+		fmt.Print(prompt)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input) // bỏ khoảng trắng đầu và  cuối 
+		if input != "" && regex.MatchString(input) { // Kiểm tra nếu chuỗi không rỗng và
+			return input
+		}
+		fmt.Println("Invalid input. Please enter a non-empty value without special characters.")
 	}
+}
+
+func inputInt(prompt string) int {
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print(prompt)
+        input, _ := reader.ReadString('\n')
+        input = strings.TrimSpace(input)
+        if value, err := strconv.Atoi(input); err == nil {
+            return value
+        }
+        fmt.Println("Invalid input. Please enter a valid integer.")
+    }
+}
+
+func inputFloat(prompt string) float32 {
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print(prompt)
+        input, _ := reader.ReadString('\n')
+        input = strings.TrimSpace(input)
+        if value, err := strconv.ParseFloat(input, 32); err == nil {
+            return float32(value)
+        }
+        fmt.Println("Invalid input. Please enter a valid float number.")
+    }
 }
 func main() {
 	var number int
@@ -98,7 +139,7 @@ func main() {
 	fmt.Println("______________________________________________")
 	fmt.Println("Enter the number!")
 	fmt.Println("1. Add students")
-	fmt.Println("2. Edit information")
+	fmt.Println("2. Modify information")
 	fmt.Println("3. Delete students")
 	fmt.Println("4. Find students by MSSV")
 	fmt.Println("5. Students have max average score")
@@ -106,9 +147,8 @@ func main() {
 	fmt.Println("_______________________________________________")
 
 	for {
-		fmt.Print("Number: ")
-		_, err := fmt.Scan(&number)
-		if err != nil || number > 6 || number < 1 {
+		number = inputInt("Number: ")
+		if number < 1 || number > 6 {
 			fmt.Println("Invalid value.")
 			continue
 		}
@@ -116,49 +156,28 @@ func main() {
 		switch number {
 		case 1:
 			fmt.Println("Add student.")
-			var name string
-			var mssv int
-			var mathScore, physicsScore, chemistryScore float32
-
-			fmt.Print("Name: ")
-			fmt.Scan(&name)
-
-			fmt.Print("Mssv: ")
-			fmt.Scan(&mssv)
-
-			fmt.Print("Math Score: ")
-			fmt.Scan(&mathScore)
-
-			fmt.Print("Physics Score: ")
-			fmt.Scan(&physicsScore)
-
-			fmt.Print("Chemistry Score: ")
-			fmt.Scan(&chemistryScore)
-
+			name := inputString("Name: ")
+			mssv := inputInt("Mssv: ")
+			mathScore := inputFloat("Math Score: ")
+			physicsScore := inputFloat("Physics Score: ")
+			chemistryScore := inputFloat("Chemistry Score: ")
+			
 			student := NewStudent(name, mssv, mathScore, physicsScore, chemistryScore)
 			classroom.AddStudent(student)
 		case 2:
-			fmt.Println("Edit information")
-			var name string
-			var mssv int
-			var mathScore, physicsScore, chemistryScore float32
-
-			fmt.Print("Mssv: ")
-			fmt.Scan(&mssv)
-
-			fmt.Print("Name: ")
-			fmt.Scan(&name)
-
-			fmt.Print("Math Score: ")
-			fmt.Scan(&mathScore)
-
-			fmt.Print("Physics Score: ")
-			fmt.Scan(&physicsScore)
-
-			fmt.Print("Chemistry Score: ")
-			fmt.Scan(&chemistryScore)
-
-			classroom.EditStudent(name, mssv, mathScore, physicsScore, chemistryScore)
+			fmt.Println("Modify information")
+			mssv := inputInt("Mssv: ")
+			student := classroom.FindStudent(mssv)
+			if student != nil{
+				name := inputString("Name: ")
+				mathScore := inputFloat("Math Score: ")
+				physicsScore := inputFloat("Physics Score: ")
+				chemistryScore := inputFloat("Chemistry Score: ")
+				studentUpdate := NewStudent(name, mssv, mathScore, physicsScore, chemistryScore)
+				classroom.ModifyStudent(student, studentUpdate)
+			}else{
+				fmt.Println("Student not found.")
+			}
 		case 3:
 			fmt.Println("Delete student.")
 			var mssv int
@@ -183,6 +202,5 @@ func main() {
 			return
 
 		}
-
 	}
 }
